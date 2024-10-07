cad1af74ee315f279ba6c3862c78149fe7ed4017
Brymahh <eseoghenabraimah@gmail.com>
2024-10-07 18:29:45 +0100

    funcs

--- funcs.py
+++ funcs.py
@@ -23,3 +23,42 @@ def gridSearchReport( estimator, X, y, pg, cv=LeaveOneOut(), rs=118 ):
         n_jobs=-1, 
         cv=cv
        )
+    est_cv.fit(X_train, y_train)
+
+
+
+
+# function to plot the residuals
+def plotResidue(estimator, X, y, rs=118):
+    """
+    Plots the fit residuals (price - predicted_price) vs. "surface" variable.
+    Params:
+        estimator: GridSearchCV, the regressor
+        X: Pandas dataframe, feature data
+        y: Pandas series, target
+        rs: int, random state
+    """    
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.3, random_state=rs) # train tets split
+
+    residue_train = y_train-estimator.predict(X_train) # make predictions
+    residue_test = y_test-estimator.predict(X_test)                                                     
+                                                          
+    fig, axe = plt.subplots(1, 2, figsize=(18,10)) 
+    axe[0].scatter( X_train["surface"], residue_train, label="train" )
+    axe[0].scatter( X_test["surface"], residue_test, label="test" )
+    axe[0].plot( [-2.3, 4.5], [0,0], "black" )
+    axe[0].set_xlabel("Scaled surface")
+    axe[0].set_ylabel("Fit residulas")
+    axe[0].legend()
+    
+    axe[1].hist(residue_test, bins=25)
+    axe[1].set_xlabel("Fit residual for test set")
+    axe[1].set_ylabel("Count")
+
+
+    plt.show()
+
+    print("mean residuals:", round(np.mean(residue_test), 2),
+          "\nstd:", round(np.std(residue_test), 2))
+    
